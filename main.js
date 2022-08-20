e15658c862e00136b31cc0b56936401be34a05e8
Chigozie Nri <chigozie@stability.ai>
2022-08-20 17:02:51 +0000

    Specify charRNN button

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
-const result = document.querySelector('#result');
 const output = document.querySelector('.output');
 const startButton = document.querySelector('#startButton');
 
 const charRNN = new ml5.charRNN('./models/games');
 
 function getGame() {
-    charRNN.generate({seed: 'Game Jam:'}, (err, r) => {output.textContent = r.sample; result.style.visibility = "visible"; console.log(r)});
+    charRNN.generate({seed: 'Game Jam:'}, (err, r) => {output.textContent = r.sample; console.log(r)});
 }
 
 startButton.addEventListener('click', getGame);
