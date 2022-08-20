9f35a54e1b9d590e86e24ae961d368def2fc86a6
Chigozie Nri <chigozie@stability.ai>
2022-08-20 17:15:42 +0000

    Add markov backend

--- main.js
+++ main.js
@@ -4,13 +4,23 @@ const markovOutput = document.querySelector('.markovOutput');
 const markovStartButton = document.querySelector('#markovStartButton');
 
 const charRNN = new ml5.charRNN('./models/games');
+const Markov = require('js-markov');
+
+var markov = new Markov();
+markov.addStates([
+    'Today is sunny',
+    'Today is rainy',
+    'The weather is sunny',
+    'The weather for tomorrow might be rainy'
+  ]);
+markov.train();
 
 function getGameCharRNN() {
     charRNN.generate({seed: 'Game Jam:'}, (err, r) => {charRNNOutput.textContent = r.sample; console.log(r)});
 }
 
 function getGameMarkov() {
-    var markovResult = 'foobar';
+    var markovResult = markov.generate();
     markovOutput.textContent = markovResult;
     console.log(markovResult)
 }
